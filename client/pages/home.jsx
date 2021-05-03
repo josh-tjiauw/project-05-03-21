3180f94d045c5d64d981c9c452f3958f11ba50b8
Josh Tjiauw <freezingmist2@gmail.com>
2021-05-03 16:17:47 -0700

    Added navbar onto components

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomeModal from '../components/modal';
+import HomeNavbar from '../components/navbar';
 
 export default function Home(props) {
   return (
     <>
+    <HomeNavbar />
     <HomeModal />
     </>
   );
