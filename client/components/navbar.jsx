88b96b08db2b045fc548dba0e1770d4178eb1e85
Josh Tjiauw <freezingmist2@gmail.com>
2021-05-04 12:35:41 -0700

    Added text styling

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -26,10 +26,6 @@ export default function HomeNavbar() {
       <Nav.Link href="/about">About</Nav.Link>
       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
         <NavDropdown.Item href="test">Test</NavDropdown.Item>
-        <NavDropdown.Item href="action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </Nav>
     <Form inline>
