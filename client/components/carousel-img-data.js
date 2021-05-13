fdaa1abbe246afc8367df4711c60bd2b5ad081ba
Josh Tjiauw <freezingmist2@gmail.com>
2021-05-13 14:40:27 -0700

    Added image data for carousel

---
+++ client/components/carousel-img-data.js
@@ -0,0 +1,18 @@
+export const carouselData = [
+  {
+    image: './carousel-1.png'
+  },
+
+  {
+    image: './carousel-2.png'
+  },
+  {
+    image: './carousel-3.png'
+  },
+  {
+    image: './carousel-4.png'
+  },
+  {
+    image: './carousel-5.png'
+  }
+];
